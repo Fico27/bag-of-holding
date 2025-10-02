2ae4c19b41a7170da33f71bf7719fe2d1602c1aa
Phil <182430206+Fico27@users.noreply.github.com>
2025-10-02 13:28:05 +0200

    added createUserValidation to post chain as well as post signup to end of chain.

--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const signupRouter = Router();
 const signupController = require("../controllers/signupController");
+const createUserValidation = require("../middleware/createUserValidation");
 
 signupRouter.get("/", signupController.getSignup);
+signupRouter.post("/", createUserValidation, signupController.postSignup);
 
 module.exports = signupRouter;
