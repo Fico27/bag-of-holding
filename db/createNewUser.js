572ccca881b478b7e4780873e49a524ea36f0b93
Phil <182430206+Fico27@users.noreply.github.com>
2025-10-02 14:07:46 +0200

    Updated createUser with try and catch block. Added error handling for a username/email already in use.

--- db/createNewUser.js
+++ db/createNewUser.js
@@ -2,14 +2,21 @@ const { PrismaClient } = require("../generated/prisma");
 const prisma = new PrismaClient();
 
 async function createUser(username, password, firstname, lastname) {
-  await prisma.user.create({
-    data: {
-      username,
-      password,
-      firstname,
-      lastname,
-    },
-  });
+  try {
+    await prisma.user.create({
+      data: {
+        username,
+        password,
+        firstname,
+        lastname,
+      },
+    });
+  } catch (error) {
+    if (error.code === "P2002") {
+      throw new Error("Username/email already exists");
+    }
+    throw error;
+  }
 }
 
 module.exports = { createUser };
