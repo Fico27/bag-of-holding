f934984ffd427edbf880242824c04feebbd1b944
Phil <182430206+Fico27@users.noreply.github.com>
2025-10-02 13:22:22 +0200

    added capablitity to add a new user via db/createNewUser and added postSignup in signup Controller to handle signups.

--- controllers/signupController.js
+++ controllers/signupController.js
@@ -1,7 +1,32 @@
+const bcrypt = require("bcryptjs");
+const createNewUser = require("../db/createNewUser");
+
 async function getSignup(req, res) {
   res.render("signup");
 }
 
+async function postSignup(req, res) {
+  const { username, password, firstname, lastname } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await createNewUser.createUser(
+      username,
+      hashedPassword,
+      firstname,
+      lastname
+    );
+
+    res.redirect("/login");
+  } catch (error) {
+    console.error("Signup Error", error);
+    res.render("signup", {
+      errors: [{ msg: "Database error", formInfo: req.body }],
+    });
+  }
+}
+
 module.exports = {
   getSignup,
+  postSignup,
 };
---
+++ db/createNewUser.js
@@ -0,0 +1,15 @@
+const { PrismaClient } = require("../generated/prisma");
+const prisma = new PrismaClient();
+
+async function createUser(username, password, firstname, lastname) {
+  await prisma.user.create({
+    data: {
+      username,
+      password,
+      firstname,
+      lastname,
+    },
+  });
+}
+
+module.exports = createUser;
