a176587bbd01eb3d7fec90947bd00dccd267d756
Phil <182430206+Fico27@users.noreply.github.com>
2025-10-03 12:07:34 +0200

    added postLogin to use passport and verify login. Added post to route. update login.ejs to post to /login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,7 +1,25 @@
+const passport = require("passport");
+
 async function getLogin(req, res) {
   res.render("login");
 }
 
+async function postLogin(req, res, next) {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      return res.status(401).render("login", {
+        errors: [{ msg: info?.message || "Invalid username/password" }],
+      });
+    }
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      return res.redirect("/dashboard");
+    });
+  })(req, res, next);
+}
+
 module.exports = {
   getLogin,
+  postLogin,
 };
--- routes/login.js
+++ routes/login.js
@@ -3,5 +3,6 @@ const loginRouter = Router();
 const loginController = require("../controllers/loginController");
 
 loginRouter.get("/", loginController.getLogin);
+loginRouter.post("/", loginController.postLogin);
 
 module.exports = loginRouter;
