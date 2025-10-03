a83641affc1515078220b356a9b81d6e904545d8
Phil <182430206+Fico27@users.noreply.github.com>
2025-10-03 14:26:12 +0200

    added authenication.js to authenicate when visiting parts of the site where user login is required.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
 const dashboardRouter = Router();
 const dashboardController = require("../controllers/dashboardController");
+const { isAuth } = require("../middleware/authentication");
 
-dashboardRouter.get("/", dashboardController.getDashboard);
+dashboardRouter.get("/", isAuth, dashboardController.getDashboard);
 
 module.exports = dashboardRouter;
