452964b6a271ae79743205e645fe28563924af9c
Phil <182430206+Fico27@users.noreply.github.com>
2025-10-04 14:15:31 +0200

    fixed logout router calling postLogout. Fixed dashboard ejs to load varaibles correctly.

--- routes/logout.js
+++ routes/logout.js
@@ -2,6 +2,6 @@ const { Router } = require("express");
 const logoutRouter = Router();
 const logoutController = require("../controllers/logoutController");
 
-logoutRouter.post("/", logoutController);
+logoutRouter.post("/", logoutController.postLogout);
 
 module.exports = logoutRouter;
