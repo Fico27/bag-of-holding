daf733b5d65e4468443b811bdecff6b82e43f838
Phil <182430206+Fico27@users.noreply.github.com>
2025-10-08 12:54:14 +0200

    Updated dashboardController to pull root folders as well as files. added items to be sorted by type.

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,16 +1,39 @@
-const db = require("../db/getfiles");
+const { getFilesByUser } = require("../db/getfiles");
+const { getRootFolders } = require("../db/getfolders");
 
 async function getDashboard(req, res) {
   try {
     const user = req.user;
-    const files = await db.getFilesByUser(user.id);
+    const [folders, files] = await Promise.all([
+      getRootFolders(user.id),
+      getFilesByUser,
+    ]);
 
-    res.render("dashboard", { user, files, errors: [] });
+    const items = [
+      ...folders.map((folder) => ({
+        type: "folder",
+        id: folder.id,
+        name: folder.name,
+        size: null,
+        date: files.createAt,
+      })),
+      ...files.map((file) => ({
+        type: "file",
+        id: file.id,
+        name: file.name,
+        size: file.size,
+        date: file.uploadTime,
+      })),
+    ];
+
+    items.sort((a, b) => a.type.localCompare(b.type));
+
+    res.render("dashboard", { user, items, errors: [] });
   } catch (error) {
     console.error(error);
     res.render("dashboard", {
       user: req.user,
-      files: [],
+      items: [],
       errors: [{ msg: "failed to load files." }],
     });
   }
