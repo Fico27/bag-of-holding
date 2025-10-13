ba02e627f91e7b0263fc97b78133e8e2dc3cbddf
Phil <182430206+Fico27@users.noreply.github.com>
2025-10-13 14:44:28 +0200

    Updated folderRoute for renaming and deleting.

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 const folderRouter = Router();
 const folderController = require("../controllers/folderController");
+const folderRepo = require("../db/folderRepo");
 const { isAuth } = require("../middleware/authentication");
 
 folderRouter.post("/", isAuth, folderController.postCreateFolder);
-
-// add folderController for downloads
 folderRouter.get("/:id/download", isAuth, folderController.downloadFolder);
+
+folderRouter.post("/:id/rename", isAuth, folderRepo.renameFolder);
+folderRouter.post("/:id/delete", isAuth, folderRepo.deleteFolderCascade);
 
 module.exports = folderRouter;
