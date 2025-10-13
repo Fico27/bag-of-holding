64d77fae176b43832b2dcbd947f56723b6d54eb3
Phil <182430206+Fico27@users.noreply.github.com>
2025-10-13 14:46:32 +0200

    Fixed folderRouter. accidently used db queries isnt of controller.

--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
 const folderRouter = Router();
 const folderController = require("../controllers/folderController");
-const folderRepo = require("../db/folderRepo");
+const folderController = require("../controllers/folderController");
 const { isAuth } = require("../middleware/authentication");
 
 folderRouter.post("/", isAuth, folderController.postCreateFolder);
 folderRouter.get("/:id/download", isAuth, folderController.downloadFolder);
 
-folderRouter.post("/:id/rename", isAuth, folderRepo.renameFolder);
-folderRouter.post("/:id/delete", isAuth, folderRepo.deleteFolderCascade);
+folderRouter.post("/:id/rename", isAuth, folderController.renameFolder);
+folderRouter.post("/:id/delete", isAuth, folderController.deleteFolder);
 
 module.exports = folderRouter;
