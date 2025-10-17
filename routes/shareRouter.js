89aab7fc6229d0d1644465f9d53764516eeb6ae1
Phil <182430206+Fico27@users.noreply.github.com>
2025-10-17 12:10:59 +0200

    Updated share router for folder downloads. Updated shared.ejs to point to the correct folder download path.

--- routes/shareRouter.js
+++ routes/shareRouter.js
@@ -5,5 +5,6 @@ const shareController = require("../controllers/shareController");
 shareRouter.get("/:id", shareController.viewShared);
 shareRouter.get("/:id/download", shareController.downloadSingleFile);
 shareRouter.get("/:id/files/:fileId/download", shareController.downloadShared);
+shareRouter.get("/:id/folder/download", shareController.downloadSharedFolder);
 
 module.exports = shareRouter;
